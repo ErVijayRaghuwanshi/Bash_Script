643b88273b791bb23e51ccf0ce4839052359139c
root <root@Raghuwanshi.localdomain>
2022-05-17 19:51:15 +0530

    Initial Commint Introduction to bash scripting

---
+++ 1_Indtroduction.sh
@@ -0,0 +1,14 @@
+#! /bin/bash                # This is the very first line of bash scripting which tells the terminal about wihc shell we are using
+# =================  1 Indtroduction =================
+
+
+
+clear                       # This line of code will clear the terminal console if any previous work is happen
+# by excuting the below command on the terminal we can see the list of installed shell on our system
+echo "Here is the list of installed shell on this machine: "
+cat /etc/shells             # In this tutorial we are focusing on only bash shell
+
+
+# this is a comment
+echo
+echo "Hello World!"          # This is the simple "Hello World!" Program which pring Hello World! on the terminal
